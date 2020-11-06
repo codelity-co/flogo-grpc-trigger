c622e48cebf3d20004df0f7e31d487ffaac2d50e
Reid Lai <reidlai@gmail.com>
2020-11-06 23:28:23 +0800

    initial version v0.1.0

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/codelity-co/flogo-grpc-trigger
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.3
+	github.com/project-flogo/core v1.1.0
+	google.golang.org/grpc v1.33.1
+)
---
+++ metadata.go
@@ -0,0 +1,114 @@
+package grpc
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+)
+
+type Settings struct {
+	Port       int    `md:"port,required"`
+	ProtoName  string `md:"protoName,required"`
+	ProtoFile  string `md:"protoFile"`
+	EnableTLS  bool   `md:"enableTLS"`
+	ServerCert string `md:"serverCert"`
+	ServerKey  string `md:"serverKey"`
+}
+
+// FromMap method of Settings
+func (s *Settings) FromMap(values map[string]interface{}) error {
+
+	var (
+		err error
+	)
+	s.Port, err = coerce.ToInt(values["port"]);
+	if err != nil {
+		return err
+	}
+
+	s.ProtoName, err = coerce.ToString(values["protoName"])
+	if err != nil {
+		return err
+	}
+
+	s.ProtoFile, err = coerce.ToString(values["protoFile"])
+	if err != nil {
+		return err
+	}
+
+	s.EnableTLS, err = coerce.ToBool(values["enableTLS"])
+	if err != nil {
+		return err
+	}
+
+	s.ServerCert, err = coerce.ToString(values["serverCert"])
+	if err != nil {
+		return err
+	}
+
+	s.ServerKey, err = coerce.ToString(values["serverKey"])
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+type HandlerSettings struct {
+	ServiceName string `md:"serviceName"`
+	MethodName  string `md:"methodName"`
+}
+
+type Output struct {
+	Params   map[string]interface{} `md:"params"`
+	GrpcData map[string]interface{} `md:"grpcData"`
+	Content  interface{}            `md:"content"`
+}
+
+type Reply struct {
+	Code int         `md:"code"`
+	Data interface{} `md:"data"`
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"params":   o.Params,
+		"grpcData": o.GrpcData,
+		"content":  o.Content,
+	}
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	var err error
+	o.Params, err = coerce.ToObject(values["params"])
+	if err != nil {
+		return err
+	}
+	o.GrpcData, err = coerce.ToObject(values["grpcData"])
+	if err != nil {
+		return err
+	}
+	o.Content = values["content"]
+
+	return nil
+}
+
+func (r *Reply) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"code": r.Code,
+		"data": r.Data,
+	}
+}
+
+func (r *Reply) FromMap(values map[string]interface{}) error {
+
+	var err error
+	if _, ok := values["code"]; ok {
+		r.Code, err = coerce.ToInt(values["code"])
+		if err != nil {
+			return err
+		}
+	}
+	r.Data, _ = values["data"]
+
+	return nil
+}
